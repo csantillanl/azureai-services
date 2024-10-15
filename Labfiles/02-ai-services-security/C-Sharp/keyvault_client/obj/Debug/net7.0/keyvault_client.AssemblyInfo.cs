bb455ab8b4c92d8ed14b296211ff1b5639597b7b
Carlos Santillan <csantillanleon@credicorpcapital.com>
2024-10-15 17:20:12 -0500

    xambios

--- Labfiles/02-ai-services-security/C-Sharp/keyvault_client/obj/Debug/net7.0/keyvault_client.AssemblyInfo.cs
+++ Labfiles/02-ai-services-security/C-Sharp/keyvault_client/obj/Debug/net7.0/keyvault_client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("keyvault_client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb11ca7814a9bef7ac8ddeb83ab04add5055527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6624e727a132b52ae813edb286ba85ff43728887")]
 [assembly: System.Reflection.AssemblyProductAttribute("keyvault_client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("keyvault_client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
